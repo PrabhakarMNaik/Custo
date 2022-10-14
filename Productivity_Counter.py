ddaa138ae67274b2e6e439040dc4031eb9d67fb4
PrabhakarMNaik <pmn2119@columbia.edu>
2022-10-14 17:55:38 +0530

    Autohide timer, better timing, beter GUI

--- Productivity_Counter.py
+++ Productivity_Counter.py
@@ -1,10 +1,12 @@
 import tkinter as tk
+from turtle import window_height
 
 
 import psutil
 import win32process
 import win32gui
 import time
+import pyautogui
 
 import csv
 import datetime
@@ -14,6 +16,11 @@ from datetime import date, timedelta
 FOLDER_PATH = "D:\Ekam\Daily_Log"
 file_name = FOLDER_PATH + "\\" + str(date.today()) + ".csv"
 
+
+# TODO:
+# 2. better time logging
+# 3. make executable
+# 4. add form for setting the log saving location
 
 def load_dict():
     try:
@@ -29,10 +36,6 @@ def load_dict():
         return dict()
 
 
-root = tk.Tk()
-root.title("Custodian")
-
-
 def active_window_process_name():
     # This produces a list of PIDs active window relates to
     process_ID = win32gui.GetForegroundWindow()
@@ -45,47 +48,69 @@ def active_window_process_name():
         return ""
 
 
+root = tk.Tk()
+root.geometry("300x70")
+prev_name = active_window_process_name()
+root.title("Custodian")
+
+
 program_dict = load_dict()
 
-def get_screen_name():
 
-    name = active_window_process_name()
+def get_screen_time(prev_name, current_name):
+    if current_name == prev_name:
+        program_dict[current_name][0] = program_dict[current_name][0] + \
+            (time.time() - program_dict[current_name][1])
+    program_dict[current_name][1] = time.time()
+
+
+def get_screen_name():
+    global prev_name
+    name = active_window_process_name().split(".")[0]
     if name != "":
         if name not in program_dict.keys():
             program_dict[name] = [0, time.time()]
         else:
-            program_dict[name][0] = program_dict[name][0] + \
-                (time.time() - program_dict[name][1])
-            program_dict[name][1] = time.time()
+            get_screen_time(prev_name, name)
 
-    prog_dict = '\n'.join(key + ":\t" + str(timedelta(seconds=value[0]))
+    prog_dict = '\n'.join(key + ":" + str(timedelta(seconds=value[0])).split(".")[0]
                           for key, value in program_dict.items())
     NumberLabel["text"] = prog_dict
-    win.after(1000, get_screen_name)
-    pass
+    prev_name = name
+    display_screen()
+    win.after(10, get_screen_name)
+
+
+def display_screen():
+    screen_width, screen_height = pyautogui.size()
+
+    x, y = pyautogui.position()
+
+    if x > screen_width - 50 and y > screen_height - 50:
+        win_width, win_height = 0, 0
+    else:
+        win_width, win_height = screen_width, screen_height
+
+    win.geometry("+%d+%d" % (win_width, win_height))
 
 
 def Start_counting():
     # I want to print the active screen name on the
     #  active screen on the foreground
+
     global win
     win = tk.Toplevel()
     global NumberLabel
-    NumberLabel = tk.Label(win, font=("", 10, "bold"), fg='white', bg="black")
-    NumberLabel.pack()
+    NumberLabel = tk.Label(win, font=("", 10, "bold"),
+                           fg='white', bg="black")
+    NumberLabel.pack(side="left")
 
-    screen_width = root.winfo_screenwidth()
-    screen_height = root.winfo_screenheight()
-
-    win.geometry("+%d+%d" % ((screen_width-win.winfo_width()) /
-                 2, (screen_height-win.winfo_height())/2))
     win.overrideredirect(1)
     win.wm_attributes('-topmost', 1)  # top window
     # background transparent.
     win.wm_attributes('-transparentcolor', win['bg'])
 
     win.after(0, lambda: get_screen_name())
-    # win.mainloop()
 
 
 def Stop_counting():
@@ -98,10 +123,10 @@ def Stop_counting():
     root.destroy()
 
 
-start_cap = tk.Button(text='start recording', command=Start_counting)
-start_cap.pack()
+start_cap = tk.Button(text='Start recording', command=Start_counting)
+start_cap.pack(pady=5)
 
 stop_cap = tk.Button(text='Stop recording', command=Stop_counting)
-stop_cap.pack()
+stop_cap.pack(pady=5)
 
 root.mainloop()
